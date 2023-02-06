e078732b80c96492e783c57eaf333544ded933b7
UmmeKulsumTumpa <bsse1307@iit.du.ac.bd>
2023-02-06 12:15:03 +0600

    code formatted

--- src/LOC/TotalPhysicalLine.h
+++ src/LOC/TotalPhysicalLine.h
@@ -1,22 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 
+const int MAX=1e4;
+
+/*
+The physical SLOC is a count of lines in the text of the program's
+source code including comment lines and, sometimes, blank lines.
+*/
+
 void countPhysicalLines()
 {
     char filename[] = "SourceCode.java"; // specify the file name here
-    char line[10000];
+    char line[MAX];
     int physicalLines = 0;
 
     FILE* file = fopen(filename, "r");
-    if (file) {
+
+    if (file){
         while (fgets(line, sizeof(line), file)) {
             physicalLines++;
         }
         fclose(file);
-    } else {
+    }else{
         printf("Error: Unable to open file %s\n", filename);
         return;
     }
 
-    printf("Physical lines: %d\n", physicalLines);
+    printf("Total physical lines of code : %d\n", physicalLines);
 }
