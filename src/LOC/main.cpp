3aa607da190cc37e5534a012849e3966ff67ddcf
UmmeKulsumTumpa <bsse1307@iit.du.ac.bd>
2023-02-06 16:47:53 +0600

    modified show screen

--- src/LOC/main.cpp
+++ src/LOC/main.cpp
@@ -5,6 +5,13 @@ using namespace std;
 
 int main()
 {
+    printf("\t*** WELCOME to CMC(Code Metric Calculator) ***\n\n\n");
+    printf("Below showing the related topic of LOC of a java source code:\n\n");
+    
     countPhysicalLines();
     countLogicalLines();
+    countCommentLines();
+
+    printf("\n\n");
+    printf("\t    *** Thank you for using CMC! ***\n\n\n");
 }
