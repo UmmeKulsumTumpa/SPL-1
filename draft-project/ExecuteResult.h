3d418d2a453d1e087b4b9b4c5abdd127a4c8977f
UmmeKulsumTumpa <bsse1307@iit.du.ac.bd>
2023-04-04 22:23:47 +0600

    added main executor header

---
+++ draft-project/ExecuteResult.h
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#include "LOC.h"
+#include "MethodLevelLOC.h"
+
+using namespace std;
+
+/*+++++++++++++++++++++++++++++ variables & methods of LOC.h +++++++++++++++++++++++++++++*/
+extern void LOCResultPrinter();
+
+/*++++++++++++++++++++++++++++ variables & methods of MethodLevelLOC.h ++++++++++++++++++++++++++*/
+
+extern int total_method; // total number of methods detected in the source code
+
+extern void locMethodBasedResult(int i);
+
+
+
+/****************************** Methods to be used *******************************/
+
+void detailedResultPrinter();
+void executeCalculation(string file_name);
+
+
+
+/**************************** method definations ****************************/
+
+void detailedResultPrinter(){
+
+    for(int i=0;i<total_method;i++){
+
+        locMethodBasedResult(i);
+    }
+}
+
+
+
+void executeCalculation(string file_name){
+    
+    CalcLOC(file_name);
+    LOCResultPrinter();
+    detailedResultPrinter();
+}
