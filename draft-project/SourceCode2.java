e68f05cf04ce888fe9dd1720abef38881c6832fe
UmmeKulsumTumpa <bsse1307@iit.du.ac.bd>
2023-05-20 12:17:06 +0600

    another test code

---
+++ draft-project/SourceCode2.java
@@ -0,0 +1,31 @@
+public class SourceCode2{
+    public static void main(String[] args) 
+    
+    
+    {
+        String str="Checking for removal";
+        System.out.println(str);
+    }
+
+    void cfg()
+    
+    
+    
+    
+    
+    
+    {
+
+
+        System.out.println();
+    }
+
+    void func1(){
+        if(12==13){System.out.println();}
+    }
+
+    void func2()
+{
+
+}
+}
