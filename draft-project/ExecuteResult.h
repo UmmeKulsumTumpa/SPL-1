4aa58447cabcf52ee51011ce52852429b2c14479
UmmeKulsumTumpa <bsse1307@iit.du.ac.bd>
2023-05-22 13:27:51 +0600

    console output added

--- draft-project/ExecuteResult.h
+++ draft-project/ExecuteResult.h
@@ -23,10 +23,69 @@ extern vector<node> method_tracer;
 /****************************** Methods to be used *******************************/
 void detailedResultPrinter();
 void executeCalculation(string file_name);
-
-
+void overAllResultPrinter();
+void userMenuProvider(string file_name);
+void methodWiseMetricResultPrinter();
+void WMCResultPrinter();
 
 /**************************** method definations ****************************/
+
+void WMCResultPrinter(){
+
+}
+
+void methodWiseMetricResultPrinter(){
+    printf("\n\n\t*** Displaying Method Level Result ***\n\n");
+    for(int i=0;i<total_method;i++){
+        if(method_tracer[i].constructor){
+            continue;
+        }
+        locMethodBasedResult(i);
+        printComplexityPerMethod(i);
+    }
+    printf("\n\n\n");
+}
+
+void userMenuProvider(string file_name){
+
+    string key_response;
+
+    do{
+        printf("\n\n\tProvided File Name: ");
+        cout << file_name << "\n\n";
+        printf("\t-> Here is the provided User Menu:\n");
+        printf("\t1. Overall Metric Result\n");
+        printf("\t2. MethodWise Metric Result\n");
+        printf("\t3. WMC Result\n");
+        printf("\t4. Exit\n");
+        printf("\n\t-> Enter your response: ");
+
+        cin >> key_response;
+
+        if(key_response=="1"){
+            overAllResultPrinter();
+        }
+        else if(key_response=="2"){
+            methodWiseMetricResultPrinter();
+        }
+        else if(key_response=="3"){
+            WMCResultPrinter();
+        }
+        else if(key_response!="4"){
+            printf("\n\tPlease check the user menu to select a valid response\n\n");
+        }
+
+    }
+    while(key_response!="4");
+
+    printf("\n\n");
+}
+
+void overAllResultPrinter(){
+    printf("\n\n\t*** Displaying OverAll Metric Result ***\n\n");
+    LOCResultPrinter();
+    printf("\n\n");
+}
 
 void detailedResultPrinter(){
 
@@ -46,7 +105,8 @@ void executeCalculation(string file_name){
     
     CalcLOC(file_name);
     calcComplexity(file_name);
-    LOCResultPrinter();
-    detailedResultPrinter();
+    //LOCResultPrinter();
+    //detailedResultPrinter();
     createControlFlowGraph();
+    //userMenuProvider(file_name);
 }
